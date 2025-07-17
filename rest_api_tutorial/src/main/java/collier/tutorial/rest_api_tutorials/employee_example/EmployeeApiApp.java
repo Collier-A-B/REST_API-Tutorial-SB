8ba3f7fc8ac44b0fb591e90819dc5d4644b960b3
Collier-A-B <benjaminac46@gmail.com>
2025-07-17 16:09:12 -0400

    Employee api example

---
+++ rest_api_tutorial/src/main/java/collier/tutorial/rest_api_tutorials/employee_example/EmployeeApiApp.java
@@ -0,0 +1,11 @@
+package collier.tutorial.rest_api_tutorials.employee_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeApiApp {
+    public static void main(String[] args) {
+		SpringApplication.run(EmployeeApiApp.class, args);
+	}
+}
